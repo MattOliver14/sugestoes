597333a2567388031a6bc50f8d82ec1f6f0ab338
Flavio Machado de Sousa <flavio.machado@helpersbrasil.com.br>
2021-10-01 11:08:31 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:sugestao/pages/MainDrawer.dart';
+import 'view/home_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      /*theme: ThemeData(
+          appBarTheme: AppBarTheme(backgroundColor:  Color(0xFF5fa7b3)),
+          primaryColor: Color(0xFF5fa7b3)),*/
+      home: HomePage(),
+    );
+  }
 }
